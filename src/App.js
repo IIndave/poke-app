68096b76a5f8ac7eab466249c0ca9f7a9abafdb0
Iindave <inigoindave@gmail.com>
2022-07-01 10:52:32 +0200

    Lista y estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [result, setResult] = React.useState([]);
+const [pokemon, setPokemon] = React.useState([]);
+const [loading, setLoading] = React.useState('true');
+const pokemones = [];
 
+useEffect(() => {fetch('https://pokeapi.co/api/v2/pokemon/?limit=802')
+    .then((response) => response.json())
+    .then((data) => setResult(
+      data.results.map((item) => {
+        fetch(item.url)
+          .then((response) => response.json())
+          .then((lista) => pokemones.push(lista));
+          setPokemon(pokemones);
+      }),
+    ));
+}, []);
+
+setTimeout(() => {
+  setLoading(false);
+}, 2000); 
+
+
+return (
+  <div className="App">
+  <div className='gallery'>
+    { loading ? (
+      <p>Loading...</p>
+    ) : (
+      pokemon.map((img, id) => (
+        <div className='cards' id={img.id} key={img.id}>
+          <div className='card'>
+            <div className={img.types[0].type.name}>
+              <img  className='shake' src={img.sprites.other['official-artwork'].front_default} alt='pokemon'/>
+            </div>
+            <div>
+              <h4 className='title'>{img.name}</h4>
+              <h5 className='tipo'>{img.types[0].type.name}</h5>
+              <div className='description'>
+                <h5 className='hp'>{img.stats[0].stat.name} : <span>{img.stats[0].base_stat}</span></h5>
+                <h5 className='attack'>{img.stats[1].stat.name} : <span>{img.stats[1].base_stat}</span></h5>
+                <h5 className='defense'>{img.stats[2].stat.name} : <span>{img.stats[2].base_stat}</span></h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))
+    )}
+  </div>
+  </div>
+  
+);
+}
 export default App;
